85008fe2dcaf63f2922c68acc2a99ad4235bfcc9
Ravi Kumar Pal <85501101+ravi483@users.noreply.github.com>
2024-10-25 12:15:19 +0530

    Updated navbar styles and cart/shop page styles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
 import { useAuth0 } from "@auth0/auth0-react";
+
 const Navbar = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const [isOpen, setIsOpen] = useState(false); // State to manage the toggle
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="navbar">
+    <div className={`navbar ${isOpen ? "open" : ""}`}>
       <div className="logo">
         <h1>Ecommerce</h1>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
       <div className="links">
         <Link to="/">Shop</Link>
@@ -18,19 +29,15 @@ const Navbar = () => {
         </Link>
         <Link>
           {isAuthenticated ? (
-            <Link>
-              <button
-                onClick={() =>
-                  logout({ logoutParams: { returnTo: window.location.origin } })
-                }
-              >
-                Log Out
-              </button>
-            </Link>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Log Out
+            </button>
           ) : (
-            <Link>
-              <button onClick={() => loginWithRedirect()}>Log In</button>
-            </Link>
+            <button onClick={() => loginWithRedirect()}>Log In</button>
           )}
         </Link>
       </div>
